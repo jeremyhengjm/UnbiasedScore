b036ac899721ea240a2a0a67440947d1c0698ac9
jeremyhengjm <jeremyhengjm@gmail.com>
2021-05-10 21:34:29 +0800

    10 May update

--- inst/logistic_diffusion/plot_sgld.R
+++ inst/logistic_diffusion/plot_sgld.R
@@ -1,10 +1,9 @@
 rm(list=ls())
 library(ggplot2)
-#setmytheme()
+setmytheme()
 
-N <- 5000
-comp <- 6
-load(sprintf("inst/logistic_diffusion/results/SGLD_%d.RData",comp))
+N <- 7500
+load("inst/logistic_diffusion/results/sgld.RData")
 
 # Parameters of the SGLD
 theta_dimension <- 4
@@ -17,18 +16,22 @@ delta <- matrix(rep(1:N, theta_dimension), ncol=N, byrow=T)
 delta <- a / (b + delta)**gam
 
 plot.df <- data.frame()
-repf <- factor(rep(comp, N))
 for (dim in 1:theta_dimension){
   trace <- parameters.df[,dim]
   running_avg <- cumsum(trace * delta[dim, ]) / cumsum(delta[dim, ])
-  plot.df <- rbind(plot.df, data.frame(iter = 1:N, theta = running_avg, rep=repf, component = factor(rep(dim, N))))
+  # plot.df <- rbind(plot.df, data.frame(iter = 1:N, theta = trace, smooth = running_avg, component = factor(rep(dim, N))))
+  plot.df <- rbind(plot.df, data.frame(iter = 1:N, theta = trace, smooth = running_avg, component = factor(rep(paste0("theta[", dim, "]"), N))))
 }
 
-g <- ggplot(plot.df) + geom_line(aes(x = iter, y = theta)) +
-  facet_wrap(~component, nrow=2, ncol=2, scales = "free_y") +
-  xlab("iteration") + ylab("components") +
-  theme_bw() + theme(strip.background=element_rect(fill="white"))
+# Plot
+g <- ggplot(plot.df) + geom_line(aes(x = iter, y = smooth)) + 
+  facet_wrap(~component, nrow = 2, ncol = 2, scales = "free_y", labeller = label_parsed) + 
+  scale_x_continuous(breaks = c(1, 2500, 5000, 7500)) +
+  xlab("iteration") + ylab("")
 g
 
-#ggsave(filename = "~/Dropbox/UnbiasedGradients/draft/arXiv-v1/ou_stoppingtimes_levels.eps",
-#plot = g, device = "eps", width = 8, height = 8)
+ggsave(filename = "~/Dropbox/UnbiasedGradients/draft/arXiv-v1/logistic_diffusion_sgld.pdf",
+      plot = g, device = "pdf", width = 14, height = 6)
+
+
+
